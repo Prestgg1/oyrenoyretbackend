d70f115fda50e9071447bd973a085134b6a0e1ff
Prestgg1 <prestgg12@gmail.com>
2024-11-13 18:57:57 +0400

    Default

---
+++ database/migrations/2024_11_06_182816_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('author')->constrained('users')->onDelete('cascade');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->text('content');
+            $table->string('slug')->nullable();
+            $table->json('tags')->nullable();
+            $table->string('thumbnail')->nullable();
+            $table->boolean('active')->default(1);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
